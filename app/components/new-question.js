15895467be7dcfe909317ad56fb5bdd923c8ef5e
John <jonnygovish@gmail.com>
2017-09-22 18:15:46 +0300

    question.js add behaviout to the data

---
+++ app/components/new-question.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() { 
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion(){
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        content: this.get('content'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
