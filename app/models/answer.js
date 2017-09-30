87e2a8568a86f889a5a6eaff8f2d99527854e2d5
John <jonnygovish@gmail.com>
2017-09-30 16:04:27 +0300

    Add the votes property  to the answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   answer: DS.attr(),
   author1: DS.attr(),
   date: DS.attr(),
-  vote: DS.attr(),
+  votes: DS.attr(),
   question: DS.belongsTo('question', { async: true}),
 
 });
