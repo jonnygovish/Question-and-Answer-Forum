7909d45609b4c7a463ebefc9bb07ba890878cd62
John <jonnygovish@gmail.com>
2017-09-30 16:02:54 +0300

    Clearing the input fields in the edit answer form

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -8,13 +8,17 @@ export default Ember.Component.extend({
     },
     saveAnswer(){
       var params = {
-        answer: this.get('answer'),
-        author1: this.get('author1'),
-        date: this.get('date'),
-        question: this.get('question')
+        answer: this.get('answer') ? this.get('answer'): '',
+        author1: this.get('author1') ? this.get('author1'): '',
+        date: this.get('date') ? this.get('date'): '',
+        question: this.get('question') ? this.get('question'): '',
+        votes: this.get('votes') ? this.get('votes'): null
       };
       this.set('addNewAnswer',false);
       this.sendAction('saveAnswer', params)
+      this.set('author1', '');
+      this.set('answer','');
+      this.set('date', '');
     }
   }
 });
